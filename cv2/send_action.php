eb318ef2af1e3dc29f3f6982ac6eeded6834da31
Eduardo Imery <ejwimery@gmail.com>
2019-03-06 17:40:28 -0600

    agregando enviar correo

---
+++ cv2/send_action.php
@@ -0,0 +1,19 @@
+<?php
+//variable from contact
+$nombre = $_POST['contacName'];
+$correo = $_POST['contactEmail'];
+$subject = $_POST['contactSubject'];
+$mensaje = $_POST['contactMessage'];
+
+//destinatary
+$destinatario ="iw.developsv@gmail.com";
+$asunto ="Message from webCv2";
+
+$carta = "De : $nombre \n";
+$carta.= "Correo: $correo \n";
+$carta.= "Subject: $subject \n";
+$carta.= "Message: $mensaje \n";
+
+// sending email
+mail($destinatario,$asunto,$carta);
+?>
