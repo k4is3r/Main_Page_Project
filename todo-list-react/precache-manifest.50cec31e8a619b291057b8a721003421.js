57fc8e8ec20c8c1fcd24243b0e440dd238a1e98c
Eduardo Imery <ejwimery@gmail.com>
2019-04-04 20:10:15 -0600

    todoList in react added

---
+++ todo-list-react/precache-manifest.50cec31e8a619b291057b8a721003421.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "01b20205a44c2eb0bf0c",
+    "url": "/static/js/main.d385dab2.chunk.js"
+  },
+  {
+    "revision": "af936525b6feb6ec8d22",
+    "url": "/static/js/2.ea6acadd.chunk.js"
+  },
+  {
+    "revision": "01b20205a44c2eb0bf0c",
+    "url": "/static/css/main.65873f6b.chunk.css"
+  },
+  {
+    "revision": "b7ac6b78b4c2e423d301a25258590724",
+    "url": "/index.html"
+  }
+];
