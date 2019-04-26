b891813c9de970c9ca8ed84328bae8d5302e2049
Eduardo Imery <ejwimery@gmail.com>
2019-04-26 00:44:32 -0600

    fixing time to link show

--- projects/NinatexProject/app.js
+++ projects/NinatexProject/app.js
@@ -12,8 +12,7 @@ const navSlide = () => {
       if (link.style.animation) {
         link.style.animation = "";
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +
-          0.8}s`;
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
       }
     });
     //animacion para burger
